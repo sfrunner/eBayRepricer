b4b5f5c5f807ab414eb2e37a94f691033f0846d8
sfrunner <sfrunner1188@gmail.com>
2017-06-15 19:48:36 -0700

    Initial commit to add to heroku

---
+++ controllers/routes.js
@@ -0,0 +1,44 @@
+﻿var express = require("express");
+var router = express.Router();
+var axios = require("axios");
+var fs = require("fs");
+var jsdom = require("jsdom");
+var request = require("request");
+var moment = require("moment");
+
+    router.get("/", function (req, res) {
+        res.render("repricer");
+    });
+
+    router.post("/sendmpns", function (req, res) {
+        console.log(__dirname);
+        console.log(req.body.data);
+        req.body.data.forEach(function (element) {
+            var query = element.substring(2,element.length);
+            var options = {
+                url: "https://api.ebay.com/buy/browse/v1/item_summary/search?q=poster+" + query + "&filter=sellers:{starcitybooks}&limit=1", 
+                headers: {
+                    "Authorization": "Bearer " + req.body.token
+                }
+            }
+            request(options, function (error, body, html) {
+                console.log(html);
+                console.log("__________");
+                var responseString = JSON.parse(html);
+                if (responseString.itemSummaries != null) {
+                    fs.appendFile("../../../../../Desktop/repricer_export_" + moment().format("MMM-Do-YYYY") + ".txt",element + "\t" + responseString.itemSummaries[0].title + "\t" + responseString.itemSummaries[0].price.value + "\r\n", function (err) {
+                        if (err) {
+                            res.send(err);
+                        }
+                        else {
+                            console.log("Saved!");
+                        }
+                    });
+                }
+            });
+        });
+            
+    });
+
+
+module.exports = router;
---
+++ public/repricer.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    var files;
+    $("#submit-btn").click(function (event) {
+        var token = $("#token").val().trim();
+        var mpns = $("#mpns").val().trim();
+        console.log(token);
+        event.preventDefault();
+        var mpnArray = [];
+        var errorArray = [];
+        mpnArray.push(mpns.split(","));
+        console.log(mpnArray);
+        $.each(mpnArray, function (i, val) {
+            console.log(val);
+            var dataPost = {
+                token: token,
+                data: val
+            }
+            $.ajax({
+                method: "POST",
+                url: "/sendmpns",
+                data: dataPost,
+                success: function (response) {
+                    console.log(response);
+                    errorArray.push(response);
+                },
+                error: function (error) {
+                    console.log(error);
+                    errorArray.push(error);
+                },
+            });
+            if (mpnArray.length === i + 1 && errorArray.length === 0) {
+                $("#status-area").html("Process is Complete!");
+            }
+            else if (mpnArray.length === i + 1 && errorArray.length > 0){
+                $("#status-area").html("Please Try Again. There was an error!");
+            }
+        });
+    });
+   
+});
